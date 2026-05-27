cfaa97ed1033adf5c17c1a62137c3474fd33a02a
root <root@DESKTOP-GH72CDF.localdomain>
2026-05-27 09:50:00 -0500

    Creacion de script en docker_postgres para crear las 14 BD (hijas) y la de Harvest

--- docker_postgres/postgres-image/docker-entrypoint-initdb.d/10_create_ckandb.sh
+++ docker_postgres/postgres-image/docker-entrypoint-initdb.d/10_create_ckandb.sh
@@ -17,6 +17,17 @@ psql -v ON_ERROR_STOP=1 --username "$POSTGRES_USER" --dbname "$POSTGRES_DB" <<-E
 EOSQL
 
 
+# 2. Crear la base de datos (con el dueño ya asignado) ARGELIA
+psql -v ON_ERROR_STOP=1 --username "$POSTGRES_USER" --dbname "$POSTGRES_DB" <<-EOSQL
+    CREATE DATABASE "$CKAN_ARGELIA_DB" OWNER "$CKAN_DB_USER" ENCODING 'UTF-8';
+EOSQL
+
+# 3. Otorgar privilegios (opcional pero recomendado)
+psql -v ON_ERROR_STOP=1 --username "$POSTGRES_USER" --dbname "$POSTGRES_DB" <<-EOSQL
+    GRANT ALL PRIVILEGES ON DATABASE "$CKAN_ARGELIA_DB" TO "$CKAN_DB_USER";
+EOSQL
+
+
 # 2. Crear la base de datos (con el dueño ya asignado) alcala
 psql -v ON_ERROR_STOP=1 --username "$POSTGRES_USER" --dbname "$POSTGRES_DB" <<-EOSQL
     CREATE DATABASE "$CKAN_ALCALA_DB" OWNER "$CKAN_DB_USER" ENCODING 'UTF-8';
@@ -27,3 +38,122 @@ psql -v ON_ERROR_STOP=1 --username "$POSTGRES_USER" --dbname "$POSTGRES_DB" <<-E
     GRANT ALL PRIVILEGES ON DATABASE "$CKAN_ALCALA_DB" TO "$CKAN_DB_USER";
 EOSQL
 
+# 2. Crear la base de datos (con el dueño ya asignado) BOLIVAR
+psql -v ON_ERROR_STOP=1 --username "$POSTGRES_USER" --dbname "$POSTGRES_DB" <<-EOSQL
+    CREATE DATABASE "$CKAN_BOLIVAR_DB" OWNER "$CKAN_DB_USER" ENCODING 'UTF-8';
+EOSQL
+
+# 3. Otorgar privilegios (opcional pero recomendado)
+psql -v ON_ERROR_STOP=1 --username "$POSTGRES_USER" --dbname "$POSTGRES_DB" <<-EOSQL
+    GRANT ALL PRIVILEGES ON DATABASE "$CKAN_BOLIVAR_DB" TO "$CKAN_DB_USER";
+EOSQL
+
+# 2. Crear la base de datos (con el dueño ya asignado) EL_AGUILA
+psql -v ON_ERROR_STOP=1 --username "$POSTGRES_USER" --dbname "$POSTGRES_DB" <<-EOSQL
+    CREATE DATABASE "$CKAN_EL_AGUILA_DB" OWNER "$CKAN_DB_USER" ENCODING 'UTF-8';
+EOSQL
+
+# 3. Otorgar privilegios (opcional pero recomendado)
+psql -v ON_ERROR_STOP=1 --username "$POSTGRES_USER" --dbname "$POSTGRES_DB" <<-EOSQL
+    GRANT ALL PRIVILEGES ON DATABASE "$CKAN_EL_AGUILA_DB" TO "$CKAN_DB_USER";
+EOSQL
+
+# 2. Crear la base de datos (con el dueño ya asignado) EL_CERRITO
+psql -v ON_ERROR_STOP=1 --username "$POSTGRES_USER" --dbname "$POSTGRES_DB" <<-EOSQL
+    CREATE DATABASE "$CKAN_EL_CERRITO_DB" OWNER "$CKAN_DB_USER" ENCODING 'UTF-8';
+EOSQL
+
+# 3. Otorgar privilegios (opcional pero recomendado)
+psql -v ON_ERROR_STOP=1 --username "$POSTGRES_USER" --dbname "$POSTGRES_DB" <<-EOSQL
+    GRANT ALL PRIVILEGES ON DATABASE "$CKAN_EL_CERRITO_DB" TO "$CKAN_DB_USER";
+EOSQL
+
+# 2. Crear la base de datos (con el dueño ya asignado) GUACARI
+psql -v ON_ERROR_STOP=1 --username "$POSTGRES_USER" --dbname "$POSTGRES_DB" <<-EOSQL
+    CREATE DATABASE "$CKAN_GUACARI_DB" OWNER "$CKAN_DB_USER" ENCODING 'UTF-8';
+EOSQL
+
+# 3. Otorgar privilegios (opcional pero recomendado)
+psql -v ON_ERROR_STOP=1 --username "$POSTGRES_USER" --dbname "$POSTGRES_DB" <<-EOSQL
+    GRANT ALL PRIVILEGES ON DATABASE "$CKAN_GUACARI_DB" TO "$CKAN_DB_USER";
+EOSQL
+
+# 2. Crear la base de datos (con el dueño ya asignado) LA_VICTORIA
+psql -v ON_ERROR_STOP=1 --username "$POSTGRES_USER" --dbname "$POSTGRES_DB" <<-EOSQL
+    CREATE DATABASE "$CKAN_LA_VICTORIA_DB" OWNER "$CKAN_DB_USER" ENCODING 'UTF-8';
+EOSQL
+
+# 3. Otorgar privilegios (opcional pero recomendado)
+psql -v ON_ERROR_STOP=1 --username "$POSTGRES_USER" --dbname "$POSTGRES_DB" <<-EOSQL
+    GRANT ALL PRIVILEGES ON DATABASE "$CKAN_LA_VICTORIA_DB" TO "$CKAN_DB_USER";
+EOSQL
+
+# 2. Crear la base de datos (con el dueño ya asignado) PRADERA
+psql -v ON_ERROR_STOP=1 --username "$POSTGRES_USER" --dbname "$POSTGRES_DB" <<-EOSQL
+    CREATE DATABASE "$CKAN_PRADERA_DB" OWNER "$CKAN_DB_USER" ENCODING 'UTF-8';
+EOSQL
+
+# 3. Otorgar privilegios (opcional pero recomendado)
+psql -v ON_ERROR_STOP=1 --username "$POSTGRES_USER" --dbname "$POSTGRES_DB" <<-EOSQL
+    GRANT ALL PRIVILEGES ON DATABASE "$CKAN_PRADERA_DB" TO "$CKAN_DB_USER";
+EOSQL
+
+# 2. Crear la base de datos (con el dueño ya asignado) RIOFRIO
+psql -v ON_ERROR_STOP=1 --username "$POSTGRES_USER" --dbname "$POSTGRES_DB" <<-EOSQL
+    CREATE DATABASE "$CKAN_RIOFRIO_DB" OWNER "$CKAN_DB_USER" ENCODING 'UTF-8';
+EOSQL
+
+# 3. Otorgar privilegios (opcional pero recomendado)
+psql -v ON_ERROR_STOP=1 --username "$POSTGRES_USER" --dbname "$POSTGRES_DB" <<-EOSQL
+    GRANT ALL PRIVILEGES ON DATABASE "$CKAN_RIOFRIO_DB" TO "$CKAN_DB_USER";
+EOSQL
+
+# 2. Crear la base de datos (con el dueño ya asignado) SAN_PEDRO
+psql -v ON_ERROR_STOP=1 --username "$POSTGRES_USER" --dbname "$POSTGRES_DB" <<-EOSQL
+    CREATE DATABASE "$CKAN_SAN_PEDRO_DB" OWNER "$CKAN_DB_USER" ENCODING 'UTF-8';
+EOSQL
+
+# 3. Otorgar privilegios (opcional pero recomendado)
+psql -v ON_ERROR_STOP=1 --username "$POSTGRES_USER" --dbname "$POSTGRES_DB" <<-EOSQL
+    GRANT ALL PRIVILEGES ON DATABASE "$CKAN_SAN_PEDRO_DB" TO "$CKAN_DB_USER";
+EOSQL
+
+# 2. Crear la base de datos (con el dueño ya asignado) TRUJILLO
+psql -v ON_ERROR_STOP=1 --username "$POSTGRES_USER" --dbname "$POSTGRES_DB" <<-EOSQL
+    CREATE DATABASE "$CKAN_TRUJILLO_DB" OWNER "$CKAN_DB_USER" ENCODING 'UTF-8';
+EOSQL
+
+# 3. Otorgar privilegios (opcional pero recomendado)
+psql -v ON_ERROR_STOP=1 --username "$POSTGRES_USER" --dbname "$POSTGRES_DB" <<-EOSQL
+    GRANT ALL PRIVILEGES ON DATABASE "$CKAN_TRUJILLO_DB" TO "$CKAN_DB_USER";
+EOSQL
+
+# 2. Crear la base de datos (con el dueño ya asignado) ULLOA
+psql -v ON_ERROR_STOP=1 --username "$POSTGRES_USER" --dbname "$POSTGRES_DB" <<-EOSQL
+    CREATE DATABASE "$CKAN_ULLOA_DB" OWNER "$CKAN_DB_USER" ENCODING 'UTF-8';
+EOSQL
+
+# 3. Otorgar privilegios (opcional pero recomendado)
+psql -v ON_ERROR_STOP=1 --username "$POSTGRES_USER" --dbname "$POSTGRES_DB" <<-EOSQL
+    GRANT ALL PRIVILEGES ON DATABASE "$CKAN_ULLOA_DB" TO "$CKAN_DB_USER";
+EOSQL
+
+# 2. Crear la base de datos (con el dueño ya asignado) VIJES
+psql -v ON_ERROR_STOP=1 --username "$POSTGRES_USER" --dbname "$POSTGRES_DB" <<-EOSQL
+    CREATE DATABASE "$CKAN_VIJES_DB" OWNER "$CKAN_DB_USER" ENCODING 'UTF-8';
+EOSQL
+
+# 3. Otorgar privilegios (opcional pero recomendado)
+psql -v ON_ERROR_STOP=1 --username "$POSTGRES_USER" --dbname "$POSTGRES_DB" <<-EOSQL
+    GRANT ALL PRIVILEGES ON DATABASE "$CKAN_VIJES_DB" TO "$CKAN_DB_USER";
+EOSQL
+
+# 2. Crear la base de datos (con el dueño ya asignado) YOTOCO
+psql -v ON_ERROR_STOP=1 --username "$POSTGRES_USER" --dbname "$POSTGRES_DB" <<-EOSQL
+    CREATE DATABASE "$CKAN_YOTOCO_DB" OWNER "$CKAN_DB_USER" ENCODING 'UTF-8';
+EOSQL
+
+# 3. Otorgar privilegios (opcional pero recomendado)
+psql -v ON_ERROR_STOP=1 --username "$POSTGRES_USER" --dbname "$POSTGRES_DB" <<-EOSQL
+    GRANT ALL PRIVILEGES ON DATABASE "$CKAN_YOTOCO_DB" TO "$CKAN_DB_USER";
+EOSQL
--- docker_postgres/postgres-image/docker-entrypoint-initdb.d/20_create_datastore.sh
+++ docker_postgres/postgres-image/docker-entrypoint-initdb.d/20_create_datastore.sh
@@ -6,12 +6,145 @@ psql -v ON_ERROR_STOP=1 --username "$POSTGRES_USER" --dbname "$POSTGRES_DB" <<-E
     CREATE ROLE "$DATASTORE_READONLY_USER" WITH NOSUPERUSER NOCREATEDB NOCREATEROLE LOGIN PASSWORD '$DATASTORE_READONLY_PASSWORD';
 EOSQL
 
-# Crear la base de datos del datastore (asignando al dueño de CKAN) ALCALA
+# Crear la base de datos del datastore (asignando al dueño de CKAN) ARGELIA
 psql -v ON_ERROR_STOP=1 --username "$POSTGRES_USER" --dbname "$POSTGRES_DB" <<-EOSQL
-    CREATE DATABASE "$DATASTORE_DB" OWNER "$CKAN_DB_USER" ENCODING 'UTF-8';
+    CREATE DATABASE "$DATASTORE_ARGELIA_DB" OWNER "$CKAN_DB_USER" ENCODING 'UTF-8';
 EOSQL
 
 # Dar permisos iniciales básicos
 psql -v ON_ERROR_STOP=1 --username "$POSTGRES_USER" --dbname "$DATASTORE_DB" <<-EOSQL
-    GRANT ALL PRIVILEGES ON DATABASE "$DATASTORE_DB" TO "$CKAN_DB_USER";
+    GRANT ALL PRIVILEGES ON DATABASE "$DATASTORE_ARGELIA_DB" TO "$CKAN_DB_USER";
 EOSQL
+
+# Crear la base de datos del datastore (asignando al dueño de CKAN) ALCALA
+psql -v ON_ERROR_STOP=1 --username "$POSTGRES_USER" --dbname "$POSTGRES_DB" <<-EOSQL
+    CREATE DATABASE "$DATASTORE_ALCALA_DB" OWNER "$CKAN_DB_USER" ENCODING 'UTF-8';
+EOSQL
+
+# Dar permisos iniciales básicos
+psql -v ON_ERROR_STOP=1 --username "$POSTGRES_USER" --dbname "$DATASTORE_DB" <<-EOSQL
+    GRANT ALL PRIVILEGES ON DATABASE "$DATASTORE_ALCALA_DB" TO "$CKAN_DB_USER";
+EOSQL
+
+
+# Crear la base de datos del datastore (asignando al dueño de CKAN) BOLIVAR
+psql -v ON_ERROR_STOP=1 --username "$POSTGRES_USER" --dbname "$POSTGRES_DB" <<-EOSQL
+    CREATE DATABASE "$DATASTORE_BOLIVAR_DB" OWNER "$CKAN_DB_USER" ENCODING 'UTF-8';
+EOSQL
+
+# Dar permisos iniciales básicos
+psql -v ON_ERROR_STOP=1 --username "$POSTGRES_USER" --dbname "$DATASTORE_DB" <<-EOSQL
+    GRANT ALL PRIVILEGES ON DATABASE "$DATASTORE_BOLIVAR_DB" TO "$CKAN_DB_USER";
+EOSQL
+
+# Crear la base de datos del datastore (asignando al dueño de CKAN) EL_AGUILA
+psql -v ON_ERROR_STOP=1 --username "$POSTGRES_USER" --dbname "$POSTGRES_DB" <<-EOSQL
+    CREATE DATABASE "$DATASTORE_EL_AGUILA_DB" OWNER "$CKAN_DB_USER" ENCODING 'UTF-8';
+EOSQL
+
+# Dar permisos iniciales básicos
+psql -v ON_ERROR_STOP=1 --username "$POSTGRES_USER" --dbname "$DATASTORE_DB" <<-EOSQL
+    GRANT ALL PRIVILEGES ON DATABASE "$DATASTORE_EL_AGUILA_DB" TO "$CKAN_DB_USER";
+EOSQL
+
+# Crear la base de datos del datastore (asignando al dueño de CKAN) EL_CERRITO
+psql -v ON_ERROR_STOP=1 --username "$POSTGRES_USER" --dbname "$POSTGRES_DB" <<-EOSQL
+    CREATE DATABASE "$DATASTORE_EL_CERRITO_DB" OWNER "$CKAN_DB_USER" ENCODING 'UTF-8';
+EOSQL
+
+# Dar permisos iniciales básicos
+psql -v ON_ERROR_STOP=1 --username "$POSTGRES_USER" --dbname "$DATASTORE_DB" <<-EOSQL
+    GRANT ALL PRIVILEGES ON DATABASE "$DATASTORE_EL_CERRITO_DB" TO "$CKAN_DB_USER";
+EOSQL
+
+# Crear la base de datos del datastore (asignando al dueño de CKAN) GUACARI
+psql -v ON_ERROR_STOP=1 --username "$POSTGRES_USER" --dbname "$POSTGRES_DB" <<-EOSQL
+    CREATE DATABASE "$DATASTORE_GUACARI_DB" OWNER "$CKAN_DB_USER" ENCODING 'UTF-8';
+EOSQL
+
+# Dar permisos iniciales básicos
+psql -v ON_ERROR_STOP=1 --username "$POSTGRES_USER" --dbname "$DATASTORE_DB" <<-EOSQL
+    GRANT ALL PRIVILEGES ON DATABASE "$DATASTORE_GUACARI_DB" TO "$CKAN_DB_USER";
+EOSQL
+
+# Crear la base de datos del datastore (asignando al dueño de CKAN) LA_VICTORIA
+psql -v ON_ERROR_STOP=1 --username "$POSTGRES_USER" --dbname "$POSTGRES_DB" <<-EOSQL
+    CREATE DATABASE "$DATASTORE_LA_VICTORIA_DB" OWNER "$CKAN_DB_USER" ENCODING 'UTF-8';
+EOSQL
+
+# Dar permisos iniciales básicos
+psql -v ON_ERROR_STOP=1 --username "$POSTGRES_USER" --dbname "$DATASTORE_DB" <<-EOSQL
+    GRANT ALL PRIVILEGES ON DATABASE "$DATASTORE_LA_VICTORIA_DB" TO "$CKAN_DB_USER";
+EOSQL
+
+
+# Crear la base de datos del datastore (asignando al dueño de CKAN) PRADERA
+psql -v ON_ERROR_STOP=1 --username "$POSTGRES_USER" --dbname "$POSTGRES_DB" <<-EOSQL
+    CREATE DATABASE "$DATASTORE_PRADERA_DB" OWNER "$CKAN_DB_USER" ENCODING 'UTF-8';
+EOSQL
+
+# Dar permisos iniciales básicos
+psql -v ON_ERROR_STOP=1 --username "$POSTGRES_USER" --dbname "$DATASTORE_DB" <<-EOSQL
+    GRANT ALL PRIVILEGES ON DATABASE "$DATASTORE_PRADERA_DB" TO "$CKAN_DB_USER";
+EOSQL
+
+# Crear la base de datos del datastore (asignando al dueño de CKAN) RIOFRIO
+psql -v ON_ERROR_STOP=1 --username "$POSTGRES_USER" --dbname "$POSTGRES_DB" <<-EOSQL
+    CREATE DATABASE "$DATASTORE_RIOFRIO_DB" OWNER "$CKAN_DB_USER" ENCODING 'UTF-8';
+EOSQL
+
+# Dar permisos iniciales básicos
+psql -v ON_ERROR_STOP=1 --username "$POSTGRES_USER" --dbname "$DATASTORE_DB" <<-EOSQL
+    GRANT ALL PRIVILEGES ON DATABASE "$DATASTORE_RIOFRIO_DB" TO "$CKAN_DB_USER";
+EOSQL
+
+# Crear la base de datos del datastore (asignando al dueño de CKAN) SAN_PEDRO
+psql -v ON_ERROR_STOP=1 --username "$POSTGRES_USER" --dbname "$POSTGRES_DB" <<-EOSQL
+    CREATE DATABASE "$DATASTORE_SAN_PEDRO_DB" OWNER "$CKAN_DB_USER" ENCODING 'UTF-8';
+EOSQL
+
+# Dar permisos iniciales básicos
+psql -v ON_ERROR_STOP=1 --username "$POSTGRES_USER" --dbname "$DATASTORE_DB" <<-EOSQL
+    GRANT ALL PRIVILEGES ON DATABASE "$DATASTORE_SAN_PEDRO_DB" TO "$CKAN_DB_USER";
+EOSQL
+
+# Crear la base de datos del datastore (asignando al dueño de CKAN) TRUJILLO
+psql -v ON_ERROR_STOP=1 --username "$POSTGRES_USER" --dbname "$POSTGRES_DB" <<-EOSQL
+    CREATE DATABASE "$DATASTORE_TRUJILLO_DB" OWNER "$CKAN_DB_USER" ENCODING 'UTF-8';
+EOSQL
+
+# Dar permisos iniciales básicos
+psql -v ON_ERROR_STOP=1 --username "$POSTGRES_USER" --dbname "$DATASTORE_DB" <<-EOSQL
+    GRANT ALL PRIVILEGES ON DATABASE "$DATASTORE_TRUJILLO_DB" TO "$CKAN_DB_USER";
+EOSQL
+
+# Crear la base de datos del datastore (asignando al dueño de CKAN) ULLOA
+psql -v ON_ERROR_STOP=1 --username "$POSTGRES_USER" --dbname "$POSTGRES_DB" <<-EOSQL
+    CREATE DATABASE "$DATASTORE_ULLOA_DB" OWNER "$CKAN_DB_USER" ENCODING 'UTF-8';
+EOSQL
+
+# Dar permisos iniciales básicos
+psql -v ON_ERROR_STOP=1 --username "$POSTGRES_USER" --dbname "$DATASTORE_DB" <<-EOSQL
+    GRANT ALL PRIVILEGES ON DATABASE "$DATASTORE_ULLOA_DB" TO "$CKAN_DB_USER";
+EOSQL
+
+# Crear la base de datos del datastore (asignando al dueño de CKAN) VIJES
+psql -v ON_ERROR_STOP=1 --username "$POSTGRES_USER" --dbname "$POSTGRES_DB" <<-EOSQL
+    CREATE DATABASE "$DATASTORE_VIJES_DB" OWNER "$CKAN_DB_USER" ENCODING 'UTF-8';
+EOSQL
+
+# Dar permisos iniciales básicos
+psql -v ON_ERROR_STOP=1 --username "$POSTGRES_USER" --dbname "$DATASTORE_DB" <<-EOSQL
+    GRANT ALL PRIVILEGES ON DATABASE "$DATASTORE_VIJES_DB" TO "$CKAN_DB_USER";
+EOSQL
+
+# Crear la base de datos del datastore (asignando al dueño de CKAN) YOTOCO
+psql -v ON_ERROR_STOP=1 --username "$POSTGRES_USER" --dbname "$POSTGRES_DB" <<-EOSQL
+    CREATE DATABASE "$DATASTORE_YOTOCO_DB" OWNER "$CKAN_DB_USER" ENCODING 'UTF-8';
+EOSQL
+
+# Dar permisos iniciales básicos
+psql -v ON_ERROR_STOP=1 --username "$POSTGRES_USER" --dbname "$DATASTORE_DB" <<-EOSQL
+    GRANT ALL PRIVILEGES ON DATABASE "$DATASTORE_YOTOCO_DB" TO "$CKAN_DB_USER";
+EOSQL
+
